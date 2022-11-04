8254e379aa8b7a88e4452389b00669520916a58f
Alwin <meijboom.am@gmail.com>
2022-11-04 20:36:20 +0100

    WIP - Velocity-based movement

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,21 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Player
 {
     public class Movement : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public float speed = 7;
+
+        private float _inputX;
+        private float _inputY;
+        private Rigidbody2D _rb;
+
+        private void Start() => _rb = GetComponent<Rigidbody2D>();
+
+        private void Update()
         {
-            
+            _inputX = Input.GetAxis("Horizontal");
+            _inputY = Input.GetAxis("Vertical");
+
+            speed = Input.GetKeyDown(KeyCode.Joystick1Button0) ? 12 : speed;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        private void FixedUpdate() => _rb.velocity = new Vector2(_inputX * speed, _inputY * speed);
     }
 }
